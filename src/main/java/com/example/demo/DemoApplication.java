71fd0281a4262d3e7a13abddcd759cc010fdcb73
Your Name <tarik18007@gmail.com>
2023-03-14 17:33:20 +0530

    First commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner CommandLineRunner(){
+		return (args)->{
+			System.out.println("Running ___");
+		};
+
+	}
+}
